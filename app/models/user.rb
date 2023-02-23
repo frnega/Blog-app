73f483b0d9f320afba01ec11229406830f8c4355
Frehiywot Nega <fr.nega@gmail.com>
2023-02-23 12:40:11 +0300

    fix to rubocop error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :likes, inverse_of: :author, foreign_key: :author_id
   has_many :posts, inverse_of: :author, foreign_key: :author_id
 
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
