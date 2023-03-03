50638ea12020bb24977c958b0693b52f48c6e98e
Frehiywot Nega <fr.nega@gmail.com>
2023-03-03 10:27:55 +0300

    make change to the user and post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false
+    else
+      @posts = @user.posts
+      render :show, locals: { user: @user, posts: @posts }
+    end
   end
 end
