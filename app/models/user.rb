1d75c71bf083cfed893f4966a13115d8e17a3e99
Frehiywot Nega <fr.nega@gmail.com>
2023-03-07 20:39:08 +0300

    added devices

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :comments, inverse_of: :author, foreign_key: :author_id
   has_many :likes, inverse_of: :author, foreign_key: :author_id
   has_many :posts, inverse_of: :author, foreign_key: :author_id
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
